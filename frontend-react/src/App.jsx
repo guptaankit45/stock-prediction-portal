5d51c5dae6028a08f1f3933dd83d801fca59e260
Ankit gupta <ankitgupta.99390@gmail.com>
2025-07-24 15:53:56 +0530

    loginfuntionality

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -6,11 +6,13 @@ import Register from './components/Register'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Login from './components/Login'
+import AuthProvider from './AuthProvider'
 
 function App() {
 
   return (
     <>
+    <AuthProvider>
       <BrowserRouter>
       <Header/>
           <Routes>
@@ -20,6 +22,8 @@ function App() {
           </Routes>
       <Footer/>
       </BrowserRouter>
+
+    </AuthProvider>
      
     </>
   )
--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import {useContext} from "react"
 import Button from "./Button"
-import { Link } from "react-router-dom"
-
+import { Link,useNavigate } from "react-router-dom"
+import { AuthContext } from "../AuthProvider"
 const Header = () => {
+
+    const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
+    const navigate = useNavigate();
+    const handleLogout = () =>{
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
+      setIsLoggedIn(false)
+      console.log('Logged out');
+      navigate('/login')
+    }
+  
  
   return (
     <>
@@ -10,9 +21,17 @@ const Header = () => {
             <Link className='navbar-brand text-light' to="/">Stock Prediction Portal</Link>
 
             <div>
+              {isLoggedIn ?(
+                <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
+              ):(
+              <>
               <Button text='Login' class = "btn-outline-info" url ="/login"/>
                 &nbsp;
                 <Button text='Register' class ="btn-info" url = "/register"/>
+                
+              </>
+            )}
+              
             </div>
         </nav>
     </>
